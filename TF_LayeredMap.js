83729b0f493934209cd1b496d27700f340fdc91b
OHGUSHI toshifumi <webmaster@tonbi.jp>
2018-12-15 15:34:43 +0900

    スクロールに対応できてなかったので、全面的に書き換え

--- TF_LayeredMap.js
+++ TF_LayeredMap.js
@@ -1,6 +1,6 @@
 //========================================
 // TF_LayeredMap
-// Version :0.0.0.0 
+// Version :0.0.1.0 
 // For : RPGツクールMV (RPG Maker MV)
 // -----------------------------------------------
 // Copyright : Tobishima-Factory 2018
@@ -34,88 +34,101 @@
  * 
  * 利用規約 : MITライセンス
  */
-
+/**
+ * 今後の予定
+ * ・2階以上の指定に対応する(ループに注意)
+ */
 (function(){'use strict';
 
 const PLUGIN_NAME = 'TF_LayeredMap';
 const pluginParams = PluginManager.parameters( PLUGIN_NAME );
 
-const _Spriteset_Map_createLowerLayer = Spriteset_Map.prototype.createLowerLayer;
-Spriteset_Map.prototype.createLowerLayer = function() {
-    _Spriteset_Map_createLowerLayer.call(this);
+/**
+ * タイルセットの画像を設定する。
+ * マップ開始時に呼ばれる。
+ */
+const _ShaderTilemaprefreshTileset = ShaderTilemap.prototype.refreshTileset;
+ShaderTilemap.prototype.refreshTileset = function() {
+    _ShaderTilemaprefreshTileset.call(this);
 
-    /**
-     * 書き割り風オブジェクトの生成
-     * @param {Tilemap} tilemap タイルマップ
-     * @param {Number} y マップ上のy座標(下位置のピクセル)
-     * @returns {RectTileLayer}
-     */
-    function makeBillboard( tilemap, y ){
-        const zLayer = new PIXI.tilemap.ZLayer( tilemap, 3 );
-        zLayer.y = y;
-        zLayer.spriteId = -Infinity;
-        tilemap.addChild( zLayer );
-        const lowerLayer = new PIXI.tilemap.CompositeRectTileLayer( 0, [], 0 );
+    // BitmapをPIXI.Textureにコンバート
+    const bitmaps = this.bitmaps.map(function(x) {
+        return x._baseTexture ? new PIXI.Texture(x._baseTexture) : x;
+   } );
 
-        const bitmaps = tilemap.bitmaps.map(function(x) {
-             return x._baseTexture ? new PIXI.Texture(x._baseTexture) : x;
-        } );
-        lowerLayer.setBitmaps( bitmaps );
-        zLayer.addChild( lowerLayer );
-        return lowerLayer.children[0];
+   // 書き割りのタイルセットの画像をアップデート
+   for( let curItem of this.TF_billboards ){
+        curItem.children[0].setBitmaps( bitmaps );
+   }
+}
+
+/**
+ * 
+ */
+const _ShaderTilemap_createLayers = ShaderTilemap.prototype._createLayers;
+ShaderTilemap.prototype._createLayers = function() {
+    _ShaderTilemap_createLayers.call(this);
+
+    // 書き割り風オブジェクトを
+    // 画面に表示される縦のタイル数+1 生成
+    // +1 はスクロールの際にはみ出す部分
+    // TODO:書き割りに高さが追加されると、さらに足す必要がある
+    const th = this._tileHeight;
+    const tileRows = Math.ceil(this._height / th) + 1;
+
+    if( !this.hasOwnProperty( 'TF_billboards' ) ){
+        this.TF_billboards = [];
     }
 
-    let setNumber = 0;
-
-    // マップ情報を取り出す
-    const flags = $gameMap.tilesetFlags();
-    const tw = $gameMap.tileWidth();
-    const th = $gameMap.tileHeight();
-    const sw = $gameMap.width();
-    const sh = $gameMap.height();
-
-    for( let y = 0; y < sh; y++ ){
-        const layer = makeBillboard(this._tilemap, tw + y * tw );
-        for( let x = 0; x < sw; x++ ){
-            for( let z = 3; z < 5; z++ ){
-                const tileId = $gameMap.tileId (x, y, z);
-                // [☆]かつ通行不可フラグが立ってる場合、以外は処理しない
-                if( ( flags[ tileId ] & 0x10 ) && ( flags[ tileId ] & 0xF ) ){
-                    this._tilemap._drawNormalTile(layer, tileId, x * tw, -th);
-                }
-            }
-        }
+    for( let i = 0; i < tileRows; i++ ){
+        const billboard = new PIXI.tilemap.ZLayer( this, 3 );
+        billboard.spriteId = -Infinity;
+        this.addChild( billboard );
+        this.TF_billboards.push( billboard );
+        const layer = new PIXI.tilemap.CompositeRectTileLayer( 0, [], 0 );
+        billboard.addChild( layer );
     }
 };
 
+/**
+ * 描画前に書き割りの中を空にしておく。
+ */
+const _ShaderTilemap_paintAllTiles = ShaderTilemap.prototype._paintAllTiles;
+ShaderTilemap.prototype._paintAllTiles = function(startX, startY) {
+    for( let curItem of this.TF_billboards ){
+        curItem.clear();
+    }
+
+    _ShaderTilemap_paintAllTiles.call( this, startX, startY );
+}
 
 /**
- * 描画
- * @method _paintTiles
- * @param {Number} startX
- * @param {Number} startY
- * @param {Number} x
- * @param {Number} y
- * @private
+ * タイルマップと書き割りの描画。
+ * 関数をまるっと書き換えているので
+ * 他のプラグインとコンフリクトを起こしてしまうので注意。
+ * @param {Number} startX 開始 マップ x座標(タイル数)
+ * @param {Number} startY 開始 マップ y座標(タイル数)
+ * @param {Number} x 画面上の x座標(タイル数)
+ * @param {Number} y 画面上の y座標(タイル数)
  */
-const _ShaderTilemap_paintTiles = ShaderTilemap.prototype._paintTiles;
+//const _ShaderTilemap_paintTiles = ShaderTilemap.prototype._paintTiles;
 ShaderTilemap.prototype._paintTiles = function( startX, startY, x, y ) {
     //_ShaderTilemap_paintTiles.call( this, startX, startY, x, y );
 
-    const mx = startX + x;
-    const my = startY + y;
-    const dx = x * this._tileWidth;
-    const dy = y * this._tileHeight;
+    const mx = startX + x; //  描画対象のマップ x座標(タイル数)
+    const my = startY + y; //  描画対象のマップ y座標(タイル数)
+    const dx = x * this._tileWidth; //  描画位置の x座標(ピクセル)
+    const dy = y * this._tileHeight; //  描画位置の y座標(ピクセル)
 
-    const tileId0 = this._readMapData( mx, my, 0 ); // 下層タイルA
-    const tileId1 = this._readMapData( mx, my, 1 ); // 下層タイルA2右側など
+    const tileId0 = this._readMapData( mx, my, 0 ); // 低層タイルA
+    const tileId1 = this._readMapData( mx, my, 1 ); // 低層タイルA2右側など
     const tileId2 = this._readMapData( mx, my, 2 ); // B 〜 E タイル
     const tileId3 = this._readMapData( mx, my, 3 ); // B 〜 E タイル
     const shadowBits = this._readMapData( mx, my, 4 ); // 影ペン
-    const upperTileId1 = this._readMapData( mx, my - 1, 1 ); // 上位置の下層タイルA
+    const upperTileId1 = this._readMapData( mx, my - 1, 1 ); // 上位置の低層タイルA
 
-    const lowerLayer = this.lowerLayer.children[ 0 ];     // 下層レイヤ(z:)
-    const upperLayer = this.upperLayer.children[ 0 ];   // 上層レイヤ
+    const lowerLayer = this.lowerLayer.children[ 0 ];     // 低層レイヤ( z: 0 )
+    const upperLayer = this.upperLayer.children[ 0 ];   // 高層レイヤ( z: 4 )
 
     /**
      * タイルを描画(upperLayer,lowerLayer,dx,dy は親の変数を使う)
@@ -124,7 +137,10 @@ ShaderTilemap.prototype._paintTiles = function( startX, startY, x, y ) {
     const drawTile = ( tileId ) => {
         if ( this._isHigherTile( tileId ) ) {
             // 全方向通行可の場合は高層マップレイヤに表示
-            if( ( this.flags[ tileId ] & 0xF ) === 0 ){
+            if( this.flags[ tileId ] & 0xF ){
+                this._drawTile( this.TF_billboards[ y ].children[ 0 ].children[ 0 ], tileId, dx, -this._tileHeight );
+                
+            }else{
                 this._drawTile( upperLayer, tileId, dx, dy );
             }
         } else {
@@ -150,6 +166,36 @@ ShaderTilemap.prototype._paintTiles = function( startX, startY, x, y ) {
         drawTile( tileId3 );
     }
 };
+
+/**
+ * (スクロールに合わせて)書き割りの表示位置を変更
+ * @param {Number} startX
+ * @param {Number} startY
+ */
+const _ShaderTilemap_updateLayerPositions = ShaderTilemap.prototype._updateLayerPositions;
+ShaderTilemap.prototype._updateLayerPositions = function( startX, startY ) {
+    _ShaderTilemap_updateLayerPositions.call( this, startX, startY );
+
+    let ox,oy;
+    if (this.roundPixels) {
+        ox = Math.floor(this.origin.x);
+        oy = Math.floor(this.origin.y);
+    } else {
+        ox = this.origin.x;
+        oy = this.origin.y;
+    }
+    const th = this._tileHeight;
+    const tw = this._tileWidth;
+    const posX = startX * tw - ox;
+    const posY = startY * th - oy
+    const l = this.TF_billboards.length;
+    for( let i = 0; i < l; i++ ) {
+        const curItem = this.TF_billboards[ i ];
+        curItem.position.x = posX;
+        curItem.position.y = posY + ( i + 1) * th;
+    };
+};
+
 
 
 /**
