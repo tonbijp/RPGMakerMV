5d1a54c41289396f196b099ac5f10c3412135c7e
OHGUSHI toshifumi <webmaster@tonbi.jp>
2019-08-13 05:52:08 +0900

    段差の段階をひとつ増やした

--- TF_Undulation.js
+++ TF_Undulation.js
@@ -1,6 +1,6 @@
 //========================================
 // TF_Undulation.js
-// Version :0.2.1.0
+// Version :0.3.0.0
 // For : RPGツクールMV (RPG Maker MV)
 // -----------------------------------------------
 // Copyright : Tobishima-Factory 2019
@@ -36,14 +36,14 @@
  * 1. A5BCDEタイルに地形タグ(デフォルト : 1)を指定
  * 
  * 2. 通行設定(4方向)によって、詳細設定
- *      0x0 ↑→←↓ : 高さレベル1(規定値:8px)
- *      0x1 ↑→←・ : 高さレベル2(規定値:16px)
+ *      0x0 ↑→←↓ : 段差レベル1(規定値:6px)
+ *      0x1 ↑→←・ : 段差レベル2(規定値:12px)
  *      0x2 ↑→・↓ : \  63°
  *      0x3 ↑→・・ : ＼ 45°
  *      0x4 ↑・←↓ :  / 63°
  *      0x5 ↑・←・ : ／ 45°
- *      0x6 ↑・・↓ : 高さレベル3(規定値:24px)
- *      0x7 ↑・・・ : 未設定
+ *      0x6 ↑・・↓ : 段差レベル3(規定値:18px)
+ *      0x7 ↑・・・ : 段差レベル4(規定値:24px)
  *      0x8 ・→←↓ : 未設定
  *      0x9 ・→←・ : 未設定
  *      0xA ・→・↓ : ＼ 27° A 上より
@@ -81,9 +81,12 @@ if( pluginParams[ TERRAIN_TAG ] ){
 
 // Wは西(左)上がり…＼ 　Eは東(右)上がり…／
 const MASK_UNDULATION = 0x001F; // 地形タグ、高層[☆]、通行設定を取り出すマスク
+// 段差設定フラグ
 const FLAG_BUMP1 = 0x0;
 const FLAG_BUMP2 = 0x1;
 const FLAG_BUMP3 = 0x6;
+const FLAG_BUMP4 = 0x7;
+// 傾き設定フラグ
 const FLAG_W45 = 0x3;
 const FLAG_E45 = 0x5;
 const FLAG_W63 = 0x2;
@@ -100,6 +103,8 @@ FLAG2RATIO_W[ FLAG_W45 ] = [ 0.2, 0.2 ]; // ↖︎
 FLAG2RATIO_W[ FLAG_E45 ] = [ 0.2, -0.2 ]; // ↙︎
 FLAG2RATIO_W[ FLAG_W63 ] = [ 0.6, 0.2 ]; // ↖︎
 FLAG2RATIO_W[ FLAG_E63 ] = [ 0.6, -0.2 ]; //  ↙︎
+FLAG2RATIO_W[ FLAG_W27A ] = [ 0.2, 0.6 ]; // ↖︎
+FLAG2RATIO_W[ FLAG_E27A ] = [ 0.2, -0.6 ]; // ↙︎
 FLAG2RATIO_W[ FLAG_W27B ] = [ 0.2, 0.6 ]; // ↖︎
 FLAG2RATIO_W[ FLAG_E27B ] = [ 0.2, -0.6 ]; // ↙︎
 const FLAG2RATIO_E = {}; // 東(右)向き→
@@ -107,6 +112,8 @@ FLAG2RATIO_E[ FLAG_W45 ] = [ -0.2, -0.2 ]; // ↘︎
 FLAG2RATIO_E[ FLAG_E45 ] = [ -0.2, 0.2 ]; // ↗︎
 FLAG2RATIO_E[ FLAG_W63 ] = [ -0.6, -0.2 ]; // ↘︎
 FLAG2RATIO_E[ FLAG_E63 ] = [ -0.6, 0.2 ]; // ↗︎
+FLAG2RATIO_E[ FLAG_W27A ] = [ -0.2, -0.6 ]; // ↘︎
+FLAG2RATIO_E[ FLAG_E27A ] = [ -0.2, 0.6 ]; // ↗︎
 FLAG2RATIO_E[ FLAG_W27B ] = [ -0.2, -0.6 ]; // ↘︎
 FLAG2RATIO_E[ FLAG_E27B ] = [ -0.2, 0.6 ]; // ↗︎
 
@@ -116,6 +123,8 @@ FLAG2POS_W[ FLAG_W45 ] = [ 0, -0.5];
 FLAG2POS_W[ FLAG_E45 ] = [ 0, 0.5];
 FLAG2POS_W[ FLAG_W63 ] = [ 0, -1];
 FLAG2POS_W[ FLAG_E63 ] = [ 0, 1];
+FLAG2POS_W[ FLAG_W27A ] = [ -0.5, -0.5];
+FLAG2POS_W[ FLAG_E27A ] = [ -0.5, 0.5];
 FLAG2POS_W[ FLAG_W27B ] = [ -0.5, -0.5];
 FLAG2POS_W[ FLAG_E27B ] = [ -0.5, 0.5];
 const FLAG2POS_E= {}; // 東(右)向き→
@@ -123,6 +132,8 @@ FLAG2POS_E[ FLAG_W45 ] = [ 0, 0.5];
 FLAG2POS_E[ FLAG_E45 ] = [ 0, -0.5];
 FLAG2POS_E[ FLAG_W63 ] = [ 0, 1];
 FLAG2POS_E[ FLAG_E63 ] = [ 0, -1];
+FLAG2POS_E[ FLAG_W27A ] = [ 0.5, 0.5];
+FLAG2POS_E[ FLAG_E27A ] = [ 0.5, -0.5];
 FLAG2POS_E[ FLAG_W27B ] = [ 0.5, 0.5];
 FLAG2POS_E[ FLAG_E27B ] = [ 0.5, -0.5];
 
@@ -365,9 +376,10 @@ Game_CharacterBase.prototype.updateMove = function() {
 
  // フラグから段差の量を得る
  const FLAG2BUMP= {};
- FLAG2BUMP[ FLAG_BUMP1 ] = 8;
- FLAG2BUMP[ FLAG_BUMP2 ] = 16;
- FLAG2BUMP[ FLAG_BUMP3 ] = 24;
+ FLAG2BUMP[ FLAG_BUMP1 ] = 6;
+ FLAG2BUMP[ FLAG_BUMP2 ] = 12;
+ FLAG2BUMP[ FLAG_BUMP3 ] = 18;
+ FLAG2BUMP[ FLAG_BUMP4 ] = 24;
 
 /**
  * 縦にずらすピクセル数を返す
