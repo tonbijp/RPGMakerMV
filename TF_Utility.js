1d3389a1a32c0f5bb28614a54ec861508a61b095
OHGUSHI toshifumi <webmaster@tonbi.jp>
2020-01-15 07:08:59 +0900

    setImagePattern を追加しコードフォーマットを整えた

--- TF_Utility.js
+++ TF_Utility.js
@@ -1,6 +1,6 @@
 //========================================
 // TF_Utility.js
-// Version :0.1.0.0
+// Version :0.2.0.0
 // For : RPGツクールMV (RPG Maker MV)
 // -----------------------------------------------
 // Copyright : Tobishima-Factory 2019
@@ -21,6 +21,14 @@
 ( function () {
 	'use strict';
 	const PLUGIN_NAME = 'TF_Utility';
+
+	/**
+	 * データベースにオリジナルのJSONを追加する
+	 */
+	// const $myJson;
+	// DataManager._databaseFiles.push(
+	// 	{ name: '$myJson', src: '$myJson.json' }
+	// );
 
 	/**
 	 * @method parseIntStrict
@@ -47,6 +55,67 @@
 	const PLAYER_CHARACTER = -1;
 	const gc = Game_Character;
 
+	/**
+	 * setImagePattern
+	 * 引数を指定しない(undefined)場合、規定値(主にコマンド実行してるイベントの現在値)が設定される
+	 * @param {Number} eventId 対象イベントID( 0:それ自身, -1:プレイヤーキャラ, -2〜-4:隊列メンバー 1〜:イベントID )
+	 * @param {String} fileName 画像ファイル名( .pngを除いた img/character/ フォルダのファイル名 )
+	 * @param {Number} charaNo キャラ番号( 画像左上から 0,１, 2, 3 と続き2段目が 4, 5, 6, 7 となる番号 )
+	 * @param {Number} patternNo パターン番号( 3パターンアニメの左から 0, 1, 2 )
+	 * @param {Number} d 向き()
+	 * @returns {Object} { id:{Number}, object:Game_Character }
+	 */
+	function setCharPattern( id, fileName, charaNo, patternNo, d ) {
+		let targetEvent;
+		// キャラクタオブジェクト(Game_Character)
+		id = ( id === undefined ) ? 0 : parseIntStrict( id );
+		if ( id < -1 ) {
+			targetEvent = $gamePlayer.followers().follower( -id );			// 隊列メンバー
+		} else {
+			targetEvent = this.character( id );			// プレイヤーキャラおよびイベント
+		}
+
+		// 画像ファイル
+		if ( fileName === undefined ) {
+			fileName = targetEvent.characterName();
+		}
+
+		// キャラ番号
+		if ( charaNo === undefined ) {
+			charaNo = targetEvent.characterIndex();
+		} else {
+			charaNo = parseIntStrict( charaNo );
+		}
+
+		targetEvent.setImage( fileName, charaNo );
+
+		// パターン番号
+		if ( patternNo !== undefined ) {
+			patternNo = parseIntStrict( patternNo );
+			targetEvent.setPattern( patternNo );
+			targetEvent._originalPattern = patternNo;
+		}
+
+		if ( d !== undefined ) {
+			// 向きを設定
+			const tmp = isDirectionFixed();
+			targetEvent.setDirectionFix( false );
+			targetEvent.setDirection( parseIntStrict( d ) );
+			targetEvent.setDirectionFix( tmp );
+		}
+
+		return { id: id, object: targetEvent };
+	}
+
+	/** ---- コマンド本体 ---- **/
+	/**
+	 * TF_setCharPattern 対象イベントID 画像ファイル名 キャラ番号 パターン番号 向き
+	 * TF_setCharPattern 2 !Door2 2 0
+	 * すべて省略可。省略したパラメータは現在設定されているものが使われる。
+	 */
+	Game_Interpreter.prototype.pluginCommandBook_TF_setCharPattern = function ( args ) {
+		setCharPattern( ...args );
+	};
 	/** ---- コマンド本体 ---- **/
 	/**
 	 * TF_verticalAnime 対象イベントID 画像ファイル名 キャラ番号 パターン番号 
@@ -54,21 +123,9 @@
 	 * すべて省略可。省略したパラメータは現在設定されているものが使われる。
 	 */
 	Game_Interpreter.prototype.pluginCommandBook_TF_verticalAnime = function ( args ) {
-		const l = args.length;
-		const eventId = ( l < 1 ) ? 0 : parseIntStrict( args[ 0 ] );
-		const targetEvent = this.character( eventId );
-		if ( 1 < l ) {
-			const fileName = args[ 1 ];
-			const charaNo = ( l < 3 ) ? targetEvent.characterIndex() : parseIntStrict( args[ 2 ] );
-			targetEvent.setImage( fileName, charaNo );
-			if ( l === 4 ) {
-				const patternNo = parseIntStrict( args[ 3 ] );
-				targetEvent.setPattern( patternNo );
-				targetEvent._originalPattern = patternNo;
-			}
-		}
-		targetEvent.setDirectionFix( false );
-		this._params = [ eventId, {
+		const result = setCharPattern.apply( this, args );
+		result.object.setDirectionFix( false );
+		this._params = [ result.id, {
 			repeat: false, skippable: true, wait: true, list: [
 				{ code: gc.ROUTE_TURN_LEFT },
 				{ code: gc.ROUTE_WAIT, parameters: [ 3 ] },
@@ -79,7 +136,7 @@
 			]
 		} ];
 		this.command205();	// SET_MOVEMENT_ROUTE
-	}
+	};
 
 
 	/**
@@ -111,7 +168,7 @@
 			{ indent: 0, code: COMMAND_END }
 		];
 		this.setupChild( commandList, this._eventId );
-	}
+	};
 
 	/**
 	 * TF_moveAfter
@@ -143,7 +200,20 @@
 			{ indent: 0, code: COMMAND_END }
 		];
 		this.setupChild( commandList, this._eventId );
-	}
+	};
+
+
+
+	// Show Picture
+	Game_Interpreter.prototype.pluginCommandBook_TF_pict = function ( args ) {
+		const x = args[ 0 ];
+		const y = args[ 1 ];
+		// 
+		$gameScreen.showPicture( this._params[ 0 ], this._params[ 1 ], this._params[ 2 ],
+			x, y, this._params[ 6 ], this._params[ 7 ], this._params[ 8 ], this._params[ 9 ] );
+		return true;
+	};
+
 
 	/**
 	 * 
@@ -165,7 +235,7 @@
 	 */
 	Game_Interpreter.prototype.pluginCommandBook_TF_switch = function () {
 		$gameSwitches.setValue( 1, $gameSwitches.getValueByName( args[ 0 ] ) );
-	}
+	};
 
 
 	/**
@@ -175,18 +245,18 @@
 	Game_Interpreter.prototype.pluginCommandBook_TF_setSelfSw = function ( args ) {
 		const type = args[ 0 ];
 		const isOn = args[ 1 ];
-		$gameSelfSwitches.setValue( [ $gameMap.mapId(), this.eventId(), type ], isOn )
+		$gameSelfSwitches.setValue( [ $gameMap.mapId(), this.eventId(), type ], isOn );
 		return this;
-	}
+	};
 	Game_Interpreter.prototype.pluginCommandBook_TF_getSelfSw = function () {
-		$gameSelfSwitches.setValue( [ $gameMap.mapId(), this.eventId(), "A" ], true )
+		$gameSelfSwitches.setValue( [ $gameMap.mapId(), this.eventId(), "A" ], true );
 		return this;
-	}
+	};
 
 	// 出現条件(アイテム)
 	Game_Interpreter.prototype.pluginCommandBook_TF_conditionItem = function () {
 		return $dataItems[ this.character( this.eventId() ).page().conditions.itemId ];
-	}
+	};
 
 	/*--- Game_Variables ---*/
 	/**
@@ -194,9 +264,9 @@
 	 */
 	Game_Variables.prototype.getValueByName = function ( name ) {
 		const i = $dataSystem.variables.findIndex( i => i === name );
-		if ( i === -1 ) throw new Error( `I can't find the variable '${ name }'` );
+		if ( i === -1 ) throw new Error( `I can't find the variable '${name}'` );
 		return this.value( i );
-	}
+	};
 
 	/*--- Game_Switches ---*/
 	/**
@@ -204,16 +274,17 @@
 	 */
 	Game_Switches.prototype.getValueByName = function ( name ) {
 		const i = $dataSystem.switches.findIndex( i => i === name );
-		if ( i === -1 ) throw new Error( `I can't find the switch '${ name }'` );
+		if ( i === -1 ) throw new Error( `I can't find the switch '${name}'` );
 		return this.value( i );
-	}
+	};
 
-/*--- Window_Options ---*/
+	/*--- Window_Options ---*/
 	/**
 	 * オプション: 音量の最小変更数を5に
 	 */
 	Window_Options.prototype.volumeOffset = function () {
 		return 5;
 	};
+
 
 } )();
