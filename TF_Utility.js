8f83de3017363913b3dbac158abd3df7b714fa8e
OHGUSHI toshifumi <webmaster@tonbi.jp>
2020-01-28 00:12:02 +0900

    アニメーション状態を各CharacterBase(イベント)に持たせる

--- TF_Utility.js
+++ TF_Utility.js
@@ -1,6 +1,6 @@
 //========================================
 // TF_Utility.js
-// Version :0.3.0.0
+// Version :0.4.0.0
 // For : RPGツクールMV (RPG Maker MV)
 // -----------------------------------------------
 // Copyright : Tobishima-Factory 2019
@@ -21,6 +21,7 @@
 ( function() {
 	'use strict';
 	const PLUGIN_NAME = 'TF_Utility';
+	const PARAM_TRUE = 'true';
 
 	/**
 	 * データベースにオリジナルのJSONを追加する
@@ -56,6 +57,18 @@
 	const gc = Game_Character;
 
 	/**
+	 * character を拡張。
+	 * @param {Number} id イベントID 
+	 */
+	function getEventById( id ) {
+		if( id < -1 ) {
+			return $gamePlayer.followers().follower( -id );			// 隊列メンバー
+		} else {
+			return this.character( id );			// プレイヤーキャラおよびイベント
+		}
+	}
+
+	/**
 	 * setImagePattern
 	 * 引数を指定しない(undefined)場合、規定値(主にコマンド実行してるイベントの現在値)が設定される
 	 * @param {Number} eventId 対象イベントID( 0:それ自身, -1:プレイヤーキャラ, -2〜-4:隊列メンバー 1〜:イベントID )
@@ -66,14 +79,9 @@
 	 * @returns {Object} { id:{Number}, object:Game_Character }
 	 */
 	function setCharPattern( id, fileName, charaNo, patternNo, d ) {
-		let targetEvent;
 		// キャラクタオブジェクト(Game_Character)
 		id = ( id === undefined ) ? 0 : parseIntStrict( id );
-		if( id < -1 ) {
-			targetEvent = $gamePlayer.followers().follower( -id );			// 隊列メンバー
-		} else {
-			targetEvent = this.character( id );			// プレイヤーキャラおよびイベント
-		}
+		const targetEvent = getEventById.call( this, id );
 
 		// 画像ファイル
 		if( fileName === undefined ) {
@@ -182,10 +190,10 @@
 	 * TF_moveAfter
 	 */
 	Game_Interpreter.prototype.pluginCommandBook_TF_moveAfter = function() {
-		// TODO: 下向きの際は、-0.5座標を移動する
 		const targetEvent = this.character( PLAYER_CHARACTER );
 		const d = targetEvent.direction();
 		if( d === 2 ) {
+			// 下向きの際は、-0.5座標を移動する
 			targetEvent._realY = targetEvent._y -= 0.5;
 		}
 
@@ -266,6 +274,23 @@
 	Game_Interpreter.prototype.pluginCommandBook_TF_conditionItem = function() {
 		return $dataItems[ this.character( this.eventId() ).page().conditions.itemId ];
 	};
+
+	/**
+	 * アニメーション開始・停止の指示。
+	 * isMoving() の判定を変えて、通常の移動をさせない。
+	 */
+	Game_Interpreter.prototype.pluginCommandBook_TF_animating = function( args ) {
+		const id = ( args[ 0 ] === undefined ) ? 0 : parseIntStrict( args[ 0 ] );
+		const targetEvent = getEventById.call( this, id );
+		targetEvent.isAnimating = args[ 1 ] ? ( args[ 1 ].toLowerCase() === PARAM_TRUE ) : true;
+	};
+	const _Game_CharacterBase_isMoving = Game_CharacterBase.prototype.isMoving;
+	Game_CharacterBase.prototype.isMoving = function() {
+		if( this.isAnimating ) return false;
+		return _Game_CharacterBase_isMoving.call( this );
+	}
+
+
 
 	/*--- Game_Variables ---*/
 	/**
@@ -354,6 +379,5 @@
 	Window_Message.prototype.newLineX = function() {
 		return $gameMessage.faceName() === '' ? 0 : ( 168 + 40 );
 	};
-
 
 } )();
