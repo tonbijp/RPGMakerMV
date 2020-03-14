de460b32cf75fdd4637ea4d52e0e8eb6ded74cdc
大串敏史 <oogushitoshifumi@iMac2F.local>
2020-03-14 18:05:52 +0900

    複数メニューに対応し、開閉アニメーションするようにした

--- TF_TextWindowMenu.js
+++ TF_TextWindowMenu.js
@@ -1,6 +1,6 @@
 //========================================
 // TF_TextWindowMenu.js
-// Version :0.1.0.0
+// Version :0.2.0.0
 // For : RPGツクールMV (RPG Maker MV)
 // -----------------------------------------------
 // Copyright : Tobishima-Factory 2020
@@ -16,51 +16,54 @@
  * @plugindesc タイトルにテキストウィンドウ表示メニューを追加。
  * @author とんび@鳶嶋工房
  *
- * @param MenuLabel
- * @desc タイトル画面でのメニュー名。
- * @type string
- * @default 著作・製作
- * 
- * @param Lines
- * @desc ウィンドウの行数。
- * @type number
- * @default 12
- * 
- * @param Contents
- * @desc ウィンドウに表示する内容(制御文字が使えます)
- * @type note
- * @default "\\}(c)KADOKAWA CORPORATION\\{"
+ * @param windowParams
+ * @desc ウィンドウの設定
+ * @type struct<WindowParamJa>[]
+ * @default ["{\"menuLabel\":\"著作・製作\",\"lines\":\"12\",\"contents\":\"\\\"\\\\\\\\}(c)KADOKAWA CORPORATION\\\\\\\\{\\\"\"}"]
  *
  * @help
+ * タイトル画面への著作権情報や操作説明の追加を想定したプラグインです。
+ * 
+ * windowParams パラメータ1行につき1メニューがタイトル画面に追加されます。
+ * そのメニューを選択すると、ウィンドウが1枚開きます。
+ * 
+ * ウィンドウの行数は lines パラメータで指定します。
+ * 
+ * contents パラメータを入力する際はコンテクストメニュー(右クリック)の
+ * [アイコンセットビューア]を利用して \I[n] の n の数値を入力できます。
+ * その他、メッセージと同じ制御文字が使えますので、ご活用ください。
  *
  * 利用規約 : MITライセンス
  */
+/*~struct~WindowParamJa:
+ *
+ * @param menuLabel
+ * @desc タイトル画面でのメニュー名。
+ * @type string
+ * @default 著作・製作
+ *
+ * @param lines
+ * @desc ウィンドウの行数。
+ * @type number
+ * @default 12
+ *
+ * @param contents
+ * @desc ウィンドウに表示する内容(制御文字が使えます)
+ * @type note
+ * @default "\\}(c)KADOKAWA CORPORATION\\{"
+ */
 ( function() {
 	'use strict';
-	const MENU_LABEL = 'MenuLabel';
-	const CONTENTS_LINES = 'Lines';
-	const CONTENTS_TEXT = 'Contents';
 	const TF_OPEN_WINDOW_COMMAND = 'TF_OPEN_WINDOW_COMMAND';
 
     /**
      * パラメータを受け取る
      */
 	const pluginParams = PluginManager.parameters( 'TF_TextWindowMenu' );
-
-	let TF_menuLabel = '著作・製作';    // メニュー名規定値
-	if( pluginParams[ MENU_LABEL ] ) {
-		TF_menuLabel = pluginParams[ MENU_LABEL ];
-	}
-
-	let TF_lines = 12;
-	if( pluginParams[ CONTENTS_LINES ] ) {
-		TF_lines = parseInt( pluginParams[ CONTENTS_LINES ], 10 );
-	}
-
-	let TF_contents = '\\}(c)KADOKAWA CORPORATION\\{';
-	if( pluginParams[ CONTENTS_TEXT ] ) {
-		TF_contents = JsonEx.parse( pluginParams[ CONTENTS_TEXT ] );
-	}
+	let TF_windows = JsonEx.parse( pluginParams.windowParams );
+	TF_windows = TF_windows.map( value => JsonEx.parse( value ) );
+	let TF_topRows;
+	let TF_itemIndex;
 
 
 	/*---- Window_TitleCommand ----*/
@@ -71,9 +74,14 @@
 	Window_TitleCommand.prototype.makeCommandList = function() {
 		_Window_TitleCommand_makeCommandList.call( this );
 
-		this.addCommand( TF_menuLabel, TF_OPEN_WINDOW_COMMAND );
+		TF_topRows = this.maxItems();
+		TF_windows.forEach( e => this.addCommand( e.menuLabel, TF_OPEN_WINDOW_COMMAND ) );
 	};
-
+	// 選択中の項目を記録
+	Window_TitleCommand.prototype.select = function( index ) {
+		Window_Command.prototype.select.apply( this, arguments );
+		TF_itemIndex = index - TF_topRows;
+	}
 
 	/*---- Scene_Title ----*/
 	/**
@@ -91,30 +99,36 @@
 
 	/*---- Scene_TF_SingleWindow ----*/
 	class Scene_TF_SingleWindow extends Scene_MenuBase {
-		constructor() {
-			super();
-		}
-
 		create() {
 			super.create();
-			this._creditWindow = new Window_Help( TF_lines );
-			this.addWindow( this._creditWindow );
-			this._creditWindow.y = ( Graphics.boxHeight - this._creditWindow.height ) / 2;
-			this._creditWindow.setText( TF_contents );
-			this._creditWindow.pause = true;
+
+			this._singleWindow = new Window_Help( parseInt( TF_windows[ TF_itemIndex ].lines, 10 ) );
+			this.addWindow( this._singleWindow );
+			this._singleWindow.y = ( Graphics.boxHeight - this._singleWindow.height ) / 2;
+			this._singleWindow.pause = true;
+			this._singleWindow.openness = 0;
+			this._singleWindow.open();
 		}
 
 		update() {
 			super.update();
-			// 入力のチェック
-			if(
-				TouchInput.isTriggered() ||
-				TouchInput.isCancelled() ||
-				Input.isTriggered( 'ok' ) ||
-				Input.isTriggered( 'cancel' )
-			) {
-				SoundManager.playCursor();
-				SceneManager.pop()
+
+			if( this._singleWindow.isOpen() ) {
+				if( !this._singleWindow._text ) {
+					const contents = JsonEx.parse( TF_windows[ TF_itemIndex ].contents );
+					this._singleWindow.setText( contents );
+				}
+				// 入力のチェック
+				if( TouchInput.isTriggered() ||
+					TouchInput.isCancelled() ||
+					Input.isTriggered( 'ok' ) ||
+					Input.isTriggered( 'cancel' )
+				) {
+					SoundManager.playCursor();
+					this._singleWindow.close();
+				}
+			} else if( this._singleWindow.isClosed() ) {
+				SceneManager.pop();
 			}
 		}
 	}
