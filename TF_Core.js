0ae75a2dd7f2c7e3843a08d1b0e8481d8e06a5e2
OHGUSHI toshifumi <webmaster@tonbi.jp>
2020-05-10 16:32:35 +0900

    よく使う関数とかまとめ
    ただし、これを使わないと動かないプラグインを作る、予定はない

---
+++ TF_Core.js
@@ -0,0 +1,25 @@
+//========================================
+// TF_Core
+// Version :0.0.0.0 
+// For : RPGツクールMV (RPG Maker MV)
+// -----------------------------------------------
+// Copyright : Tobishima-Factory 2020
+// Website : http://tonbi.jp
+//
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+//========================================
+/*:
+ * 
+ * @help
+ * プラグインで共通して使っている処理をメモ的にまとめたもので、
+ * 
+ * 利用規約 : MITライセンス
+ */
+
+( function() {
+    'use strict';
+
+} )();
+
+
